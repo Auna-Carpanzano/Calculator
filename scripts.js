51d13e66e3698efe553621838a034e4b5c76ee91
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-29 16:40:35 -0700

    Declare total variable

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   //Keeps track of state after equal button is pressed
   var afterEqual= false;
+
   //Stores result after equal button is pressed
   var lastTotal;
+
   //Stores inputs from user
   var inputs = [""];
+
+  //Stores string of inputs
+  var total;
 
 }); //End document ready function
