f68f1669a57c1a6a9ad07868bcac6ba43fb2d09c
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-29 16:42:53 -0700

    Set operators2 variable to be array of decimal

--- scripts.js
+++ scripts.js
@@ -14,4 +14,7 @@ $(document).ready(function() {
   //For validation- without decimal
   var operators1 = ["+", "-", "/", "*"];
 
+  //For validation- decimal
+  var operators2 = ["."];
+
 }); //End document ready function
