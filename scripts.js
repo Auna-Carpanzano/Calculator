099adc3844bdf775a00e83cc63f0663d49a7b839
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2017-12-29 16:38:58 -0700

    Declare lastTotal variable

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   //Keeps track of state after equal button is pressed
   var afterEqual= false;
+  //Stores result after equal button is pressed
+  var lastTotal;
 
 }); //End document ready function
